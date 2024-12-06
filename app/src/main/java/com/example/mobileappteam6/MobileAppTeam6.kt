f25b2f983b6165efebc4815dac05c394a3c3c2bb
choiyukyung <121738855+choiyukyung@users.noreply.github.com>
2024-12-06 19:57:04 +0900

    Remove previous project

--- app/src/main/java/com/example/mobileappteam6/MobileAppTeam6.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mobileappteam6
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MobileAppTeam6: Application() {
-}
--- app/src/main/java/com/example/mobileappteam6/feature/model/Essentials.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mobileappteam6.feature.model
-
-data class Essentials(
-    val id: String = "",
-    val userEmail: String = "",
-    val what: String = "",
-    val where: String = "",
-    val price: String = ""
-)
